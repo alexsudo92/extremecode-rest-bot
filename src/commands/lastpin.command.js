8721124386851bc1dfd55bc0441f0ccb71569a29
Artem Dontsov <extremecodetv@gmail.com>
2018-06-17 16:50:24 +0700

    Последнее время закрепленного сообщения (#1)
    * Add moment  package
    
    * Add lastpin command

---
+++ src/commands/lastpin.command.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+const { bot } = require('./../config');
+
+moment.locale('ru');
+
+module.exports = (() => {
+    let lastTime = moment();
+    bot.on('message', async (msg) => {
+        if (msg.pinned_message) {
+            lastTime = moment();
+        }
+    });
+
+    bot.onText(/\/lastpin/, async (msg, match) => {
+        const chatId = msg.chat.id;
+        const messageId = msg.message_id;
+
+        const delta = moment(moment()).diff(moment(lastTime));
+        const duration = moment.duration(delta);
+        const humanized = duration.humanize();
+
+        const message = `[Информативно] 
+Мешки с мясом из группировки 'Администраторы' в последний раз закрепляли сообщение ${humanized} назад`;
+
+        await bot.sendMessage(chatId, message, {
+            reply_to_message_id: messageId
+        });
+    });
+})();
